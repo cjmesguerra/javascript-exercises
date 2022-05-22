14bf812b0ba127e740ed1f11a84488a06a787fa2
Chris <christerjohnesguerra@gmail.com>
2022-05-22 17:19:00 +0800

    Complete 01 and 02 javascript-exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,21 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let text = string;
+    let number = num;
 
+    if (number === 'null' || number < 0) {
+        let result = 'ERROR';
+        return result;
+    } else {
+        let result = text.repeat(number);
+        return result;
+    }
+  
 };
 
+repeatString('hey', 3); // test 1
+repeatString('hey', 10); // test 2
+repeatString('hey', 1); // test 3
+repeatString('hey', 0); // test 4
+repeatString('hey', -1); // test 5
 // Do not edit below this line
 module.exports = repeatString;
